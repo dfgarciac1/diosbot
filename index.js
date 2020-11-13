8a28995eacf4b5eb1b39534d27fb1e3fa3e17f80
David Garcia <ticaspaspi@gmail.com>
2020-11-12 21:12:45 -0500

    Vamos

---
+++ index.js
@@ -0,0 +1,154 @@
+const { Client, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const client = new Client();
+const dotenv = require('dotenv');
+
+
+
+
+client.on('ready', () => {
+    console.log('Bot Now connected!');
+    console.log('Logged In as', client.user.tag)
+    client.user.setStatus('dnd'); // online, idle, invisible, dnd
+
+    console.log('Bot status: ', client.user.presence.status);
+
+    // Bot sending a Message to a text channel called test
+    const testChannel = client.channels.find(x => x.name === 'test')
+    console.log(testChannel)
+    // client.channels.find(c => c.name === 'test').send('Hello Server!')
+
+});
+
+// Bot listenning messages
+client.on('message', async msg => {
+    console.log(msg.content)
+    const querystring = require('querystring');
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+    if (msg.content === 'cat') {
+        const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+
+	    msg.channel.send(file);
+    }
+    if (msg.content === 'Java') {
+        const { file } = await fetch().then(response => response.json());
+
+	    msg.channel.send(file);
+    }
+    
+    if (msg.content === 'pong') {
+        msg.reply('ping')
+    }
+    if (msg.content === 'help') {
+        msg.reply('Los comandos son hello,!test,!puto,!suicide,!JS,!pretty,React')
+    }
+
+    if (msg.content === 'hello') {
+        msg.channel.send(`Hello ${msg.author}`);
+    }
+
+    if (msg.content.includes('!test')) {
+        msg.channel.send('Este grupo es de dioses');
+    }
+    if (msg.content.includes('!JS')) {
+        msg.channel.send('Viva el lenguaje ');
+        
+    }
+    
+
+    if (msg.content === '!puto') {
+        msg.channel.send('el que no cante');
+        msg.channel.send('en diciembre');
+    }
+    if (msg.content === '!Hijo') {
+        msg.channel.send('Te quiero Papa');
+        msg.channel.send('Gracias por crearme');
+    }
+    if (msg.content === '!Hijo') {
+        msg.channel.send('Te quiero Papa');
+        msg.channel.send('Gracias por crearme');
+    }
+    if (msg.content === '!suicide') {
+        msg.channel.send('Se murio');
+        msg.channel.send('El semestre');
+    }
+   
+    if (msg.content === 'ping') {
+        msg.reply('pong')
+    }
+  
+   if (msg.content === 'Github') {
+     msg.channel.send({embed: {
+        color: 3447003,
+        author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL()
+        },      
+        title: "GitHub De un Dios",
+      url: "https://github.com/dfgarciac1/React",
+      description: "Este es mi github de dioses.",
+      }
+  });
+     
+}
+
+if (msg.content === 'React') {
+    msg.channel.send({embed: {
+       color: 3447003,
+       author: {
+           name: client.user.username,
+           icon_url: client.user.avatarURL()
+       },      
+       title: "React Principio",
+       image: {
+		url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png',
+	},
+     url: "https://es.reactjs.org/docs/getting-started.html",
+     description: "React Inicios.",
+     }
+  
+ });
+    
+}
+if (msg.content === 'ReactComponente') {
+    msg.channel.send({embed: {
+       color: 3447003,
+       author: {
+           name: client.user.username,
+           icon_url: client.user.avatarURL()
+       },      
+       title: "React Principio",
+       image: {
+		url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png',
+	},
+     url: "https://es.reactjs.org/docs/getting-started.html",
+     description: "React Inicios.",
+     }
+  
+ });
+    
+}
+    
+    
+    // Deleting 100 messages
+    if (msg.content.startsWith('!clean')) {
+        async function clear() {
+            try {
+                // await msg.delete();
+                const fetched = await msg.channel.fetchMessages({limit: 99});
+                msg.channel.bulkDelete(fetched);;
+                console.log('Messages deleted');
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        clear();
+    }
+});
+
+
+const token = 'Nzc2NTE4ODk5MDI1OTY5MjA0.X62DlA.e_amPyxQ9GGpgBZfdixQWs4rlQ8';
+client.login(token);
